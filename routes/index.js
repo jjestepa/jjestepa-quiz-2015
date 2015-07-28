38fcdf8181656eccedac074e3aaa7e620ed00538
jjestepa <jesus.j.estepa@gmail.com>
2015-07-28 14:41:06 -0400

    autoload

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //Autoload :quizId
+
 /* GET /quizes/question */
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 
